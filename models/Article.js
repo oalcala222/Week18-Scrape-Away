25f7df34f4f9fea3cf4bb7873f695a0f0425ec2e
taco time <oalcala222@gmail.com>
2019-07-30 17:33:10 -0700

    still trying to fix it

--- models/Article.js
+++ models/Article.js
@@ -11,17 +11,22 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
+  // `short` summay is required and type of string
+  summary: {
+    type: String,
+    required: true
+  },
   // `link` is required and of type String
   link: {
     type: String,
     required: true
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: {
+  // `Comment` is an object that stores a Comment id
+  // The ref property links the ObjectId to the Comment model
+  // This allows us to populate the Article with an associated Comment
+  comment: {
     type: Schema.Types.ObjectId,
-    ref: "Note"
+    ref: "Comment"
   }
 });
 
--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 // ==============Dependencies=============== //
 var express = require("express");
-var path = require("path")
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// ==========Our scraping tools============= //
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-// ===========Require all models============ //
-var db = require("./models");
-
 // =====Associate our server to a port====== //
-var PORT = 3000;
+var PORT = 8080;
 
 // ===========Initialize Express============ //
 var app = express();
@@ -24,7 +14,7 @@ var app = express();
 app.use(express.static("public"));
 // Parse request body as JSON
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 
@@ -39,17 +29,16 @@ app.set("view engine", "handlebars");
 
 // ============Database Connection========== //
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 
 // =================Routes================== //
 // Import the route/controller combo and give the server access to them.
-require("./controllers/scrapecontroller.js")(app);
+require ("./controllers/scrapecontroller.js")(app);
+
 
 
 // ============Start the server============= //
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
-  
-module.exports = app;
